5aa22aab14b86079a51943df17290b6d194445e2
mccafee025 <ademaamanka@gmail.com>
2021-09-15 01:30:23 +0300

    redesign page, add jquery link, add Images, Add footer, add script

---
+++ Js/app.js
@@ -0,0 +1,47 @@
+  
+$(document).ready(function () {
+    $("#fun").toggle();
+  })
+
+
+
+$(document).ready(function () {
+  $("#pop").on("click", function () {
+    $("#fun").toggle();
+    $("#kali").toggle();
+  });
+});
+
+
+  
+$(document).ready(function () {
+  $("#speed").toggle();
+})
+
+
+
+$(document).ready(function () {
+$("#hour").on("click", function () {
+  $("#speed").toggle();
+  $("#jump").toggle();
+});
+});
+
+
+
+
+
+
+
+$(document).ready(function () {
+  $("#zebra").toggle();
+})
+
+
+
+$(document).ready(function () {
+$("#sky").on("click", function () {
+  $("#zebra").toggle();
+  $("#how").toggle();
+});
+});
